ecd3aa15bc1abf9ca44e3de3828a8e0f8c698044
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-08-08 11:07:32 -0700

    initial hbs and route setup

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
 const app = express();
+
+// Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Routes
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 3000;
 
